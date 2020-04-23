67875eeee4f88595a7e8ac4f2f2bda8f5e56c859
10098 <chanhee.won@sk.com>
2020-04-23 11:34:30 +0900

    거의 마지막

--- src/main/java/com/example/pay/external/SpaceService.java
+++ src/main/java/com/example/pay/external/SpaceService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "space", url = "http://localhost:8083/space")
+@FeignClient(name = "space", url = "${api.url.space}")
 public interface SpaceService {
     @RequestMapping(method = RequestMethod.GET, path = "/permission")
     boolean getPermission();
